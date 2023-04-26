59a2532af013434fba147a8e8201e2020c1c16bf
rookieWJ <1067940264@qq.com>
2023-04-26 11:33:20 +0800

    修改设置保存方式为localStorage

--- quasar.config.js
+++ quasar.config.js
@@ -124,13 +124,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [
-        'BottomSheet',
-        'Notify',
-        'LocalStorage',
-        'SessionStorage',
-        'AddressbarColor',
-      ],
+      plugins: ['BottomSheet', 'Notify', 'LocalStorage', 'AddressbarColor'],
     },
 
     // animations: 'all', // --- includes all animations
